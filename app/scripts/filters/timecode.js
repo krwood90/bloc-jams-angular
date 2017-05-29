b864abdf5da6fe2e32a37dcc95d03e5836296924
Kevin Wood <krwood90@gmail.com>
2017-05-29 11:32:59 -0500

    angular 11 assignment completed

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,23 +1,12 @@
 (function() {
-    function timecode() {
+    function timecode(SongPlayer) {
         return function(seconds) {
-            var seconds = Number.parseFloat(seconds);
 
             if (Number.isNaN(seconds)) {
                 return '-:--';
             }
 
-            var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
-
-            var output = minutes + ':';
-
-            if (remainingSeconds < 10) {
-                output += '0';
-            }
-
-            output += remainingSeconds;
+            var output = buzz.toTimer(seconds)
 
             return output;
         };
@@ -25,5 +14,5 @@
 
     angular
         .module('blocJams')
-        .filter('timecode', timecode);
+        .filter('timecode', ['SongPlayer', timecode]);
 })();
